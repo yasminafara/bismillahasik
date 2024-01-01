a10602fa13fae4afefbd227757326d7b7c2fdf1c
yasminafara <118978476+yasminafara@users.noreply.github.com>
2024-01-01 22:38:45 +0700

    Update banjir.py

--- banjir.py
+++ banjir.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 import streamlit as st
-
+model = pickle.load(open('svm.sav,'rb'))
 st.title('Prediksi Kenaikan Permukaan Air Laut Terhadap Banjir')                         
 
 TinggiLaut = st.text_input('Ketinggian Laut')      
@@ -9,4 +9,11 @@ KecepatanAngin = st.text_input('Kecepatan Angin')
 GangguanCuaca = st.text_input('Gangguan Cuaca')      
 Astronomi = st.text_input('Astronomi')   
 
- 
+  prediksibanjir = ''
+if st.button('Prediksi Kenaikan Permukaan Laut Terhadap Banjir'):
+    prediksibanjir = model.predict([[TinggiLaut, KecepatanAngin, GangguanCuaca, Astronomi]])
+    if (prediksibanjir[0]==1):
+        prediksibanjir = 'Terprediksi Kenaikan Permukaan Laut berpotensi Banjir'
+    else:
+         prediksibanjir = 'Terprediksi Kenaikan Permukaan Laut berpotensi Banjir'
+st.success(prediksibanjir)   
