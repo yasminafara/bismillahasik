2806808f0f0c6ee693d8f0f5abceb62b25371859
yasminafara <118978476+yasminafara@users.noreply.github.com>
2024-01-02 11:54:08 +0700

    Update banjir.py

--- banjir.py
+++ banjir.py
@@ -1,7 +1,8 @@
 import pickle
 import numpy as np
 import streamlit as st
-
+from keras.models import load_model
+model = load_model('svm1.sav')
 st.title('Prediksi Kenaikan Permukaan Air Laut Terhadap Banjir')                         
 
 TinggiLaut = st.text_input('Ketinggian Laut')      
